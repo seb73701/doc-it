e5f87617fffaadab7e10cf60ae82dc4fad545f14
root <seb73701@free.fr>
2025-08-22 21:35:03 +0200

    Initial Project

---
+++ docs/test1/test1.md
@@ -0,0 +1,40 @@
+---
+description: Créer une page de documentation avec un contenu riche.123
+image: https://i.imgur.com/mErPwqL.png
+keywords: 
+    - test1
+    - test
+tags: [docustest1aurus]
+---
+
+# doc n02
+
+Êtes-vous prêt à créer le site de documentation de votre projet open source ?
+
+## Les titres
+
+apparaîtront dans la table des matières en haut à droite
+
+Ainsi, vos utilisateurs sauront de quoi il s'agit sans avoir à faire défiler la page ou même sans la lire.
+
+## Par défaut, seuls les h2 et h3 figureront dans la table des matières.
+
+Vous pouvez configurer les niveaux de titres de la table des matières soit par document, soit dans la configuration du thème.
+
+Les titres sont bien espacés de manière à ce que la hiérarchie soit claire.
+
+- Les listes vous aideront à
+- présenter les points clés
+- que vous voulez que vos utilisateurs retiennent
+  - et vous pouvez les imbriquer
+    - plusieurs fois
+
+![test](https://53.fs1.hubspotusercontent-na1.net/hubfs/53/assets/hubspot.com/web-team/WBZ/HubSpot%20Media/Blog%202021/Images/blog-categories/mktg-medium-350px.png)
+
+```mermaid
+graph TD;
+    A-->B;
+    A-->C;
+    B-->D;
+    C-->D;
+```
---
+++ docusaurus.config.js
@@ -0,0 +1,173 @@
+// @ts-check
+// `@type` JSDoc annotations allow editor autocompletion and type checking
+// (when paired with `@ts-check`).
+// There are various equivalent ways to declare your Docusaurus config.
+// See: https://docusaurus.io/docs/api/docusaurus-config
+
+import {themes as prismThemes} from 'prism-react-renderer';
+
+// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Doc-IT',
+  tagline: 'Bienvenue sur mon blog.',
+  favicon: 'img/favicon.png',
+
+  // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
+  future: {
+    v4: true, // Improve compatibility with the upcoming Docusaurus v4
+  },
+
+  // Set the production url of your site here
+  url: 'https://seb73701.github.io',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'seb73701', // Usually your GitHub org/user name.
+  projectName: 'doc-it', // Usually your repo name.
+  trailingSlash: false,
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'fr',
+    locales: ['fr'],
+  },
+  themes: ['@docusaurus/theme-mermaid'],
+  markdown: {
+    mermaid: true,
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: './sidebars.js',
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        blog: {
+          showReadingTime: true,
+          feedOptions: {
+            type: ['rss', 'atom'],
+            xslt: true,
+          },
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          // Useful options to enforce blogging best practices
+          onInlineTags: 'warn',
+          onInlineAuthors: 'warn',
+          onUntruncatedBlogPosts: 'warn',
+        },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      // Replace with your project's social card
+      image: 'img/docusaurus-social-card.jpg',
+      docs: {
+        sidebar: {
+          hideable: true,
+          autoCollapseCategories: true,
+        },
+      },
+      navbar: {
+        title: 'Doc-IT',
+        logo: {
+          alt: 'Doc-IT Logo',
+          src: 'img/logo.png',
+        },
+        items: [
+          {
+            type: 'docSidebar',
+            sidebarId: 'docSidebar',
+            position: 'left',
+            label: 'Documentation',
+          },
+          {
+            type: 'search',
+            position: 'right',
+          },
+          {to: '/blog', label: 'Blog', position: 'left'},
+          {
+            href: 'https://github.com/seb73701',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Documentation',
+                to: '/docs/intro',
+              },
+            ],
+          },
+          {
+            title: 'Communauté',
+            items: [
+              {
+                label: 'Stack Overflow',
+                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+              },
+              {
+                label: 'Discord',
+                href: 'https://discordapp.com/invite/docusaurus',
+              },
+              {
+                label: 'X',
+                href: 'https://x.com/docusaurus',
+              },
+            ],
+          },
+          {
+            title: 'Plus',
+            items: [
+              {
+                label: 'Blog',
+                to: '/blog',
+              },
+              {
+                label: 'GitHub',
+                href: 'https://github.com/facebook/docusaurus',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} Doc-IT. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: prismThemes.github,
+        darkTheme: prismThemes.dracula,
+        defaultLanguage: 'bash',
+        additionalLanguages: ['powershell','apacheconf','asciidoc','awk','bash','batch','csp','csv','diff','dns-zone-file','docker','git','http','hsts','ignore','ini','jq','keepalived','log','markup-templating','mermaid','mongodb','nginx','perl','php','plant-uml','plsql','promql','properties','puppet','regex','rest','ruby','shell-session','sql','systemd','toml','typescript','typoscript','uri','vim','wiki'],
+      },
+    }),
+};
+
+export default config;
